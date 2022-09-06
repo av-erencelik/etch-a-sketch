4fa521a7cce36ada1b04ce5f8c297ca8f9c81b04
Mehmet Eren Çelik <erenmemo05@gmail.com>
2022-09-06 17:13:29 +0300

    apply button function

--- scripts.js
+++ scripts.js
@@ -2,16 +2,15 @@ let container = document.getElementById("grid")
 
 
 
+let applybtn = document.getElementById("apply").addEventListener("click", () => {
+    getDivs(parseInt(document.querySelector('#volume').textContent))
+})
 
 
 
 
 
 
-
-
-
-getDivs(100)
 
 
 
@@ -28,12 +27,14 @@ function outputUpdate(vol) {
 
 
 function getDivs(gridNumber) {
+    document.querySelectorAll(".square").forEach(e => e.remove())
     let side = 750 / gridNumber
     for (let i = 1; i <= gridNumber*gridNumber; i++) {
         const grid = document.createElement("div")
         grid.style.width = `${side-2}px`;
         grid.style.height = `${side-2}px`;
         grid.style.border = "1px solid black"
+        grid.classList.add("square")
         container.appendChild(grid)
     }
    
